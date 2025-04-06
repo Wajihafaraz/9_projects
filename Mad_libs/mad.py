23038acb69104f38e453b7d5e1c73225f0b9e522
Wajihafaraz <164238955+Wajihafaraz@users.noreply.github.com>
2025-04-06 19:52:33 +0500

    Add files via upload

---
+++ Mad_libs/mad.py
@@ -0,0 +1,8 @@
+school=input("School: ")
+
+College = input("College: ")
+University = input("university: ")
+
+
+Mad_lib=(f"I have done Matric from {school} and done FSC from {College}, Now I am doing B.ed from {University}  ")
+print(Mad_lib)
